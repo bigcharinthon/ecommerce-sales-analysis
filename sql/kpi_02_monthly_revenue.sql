da714944c0175227bab60557200f8ee41f20e79f
bigcharinthon <bigcharinthon124@gmail.com>
2026-03-11 12:57:52 +0700

    Update kpi_02_monthly_revenue.sql

--- sql/kpi_02_monthly_revenue.sql
+++ sql/kpi_02_monthly_revenue.sql
@@ -3,11 +3,15 @@
    Business Question:
    - What is the total revenue by month?
    =============================== */
-SELECT
-    strftime('%Y-%m', o.order_date) AS month,
-    SUM(oi.quantity * oi.price) AS monthly_revenue
-FROM orders o
-JOIN order_items oi
-    ON o.order_id = oi.order_id
-GROUP BY month
-ORDER BY month;
+
+SELECT strftime('%Y-%m', order_date) AS order_month,
+sum(total_sales) AS monthly_revenue
+from orders
+GROUP BY order_month
+ORDER by order_month;
+
+/*
+Insight:
+This query shows the monthly revenue trend, allowing us to track
+sales performance over time.
+*/

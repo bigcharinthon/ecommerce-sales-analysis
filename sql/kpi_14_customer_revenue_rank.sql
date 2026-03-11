3d1cde7ece0eb22e473c54e445f2e2bb249f76a6
bigcharinthon <bigcharinthon124@gmail.com>
2026-03-11 14:47:16 +0700

    Update kpi_14_customer_revenue_rank.sql

--- sql/kpi_14_customer_revenue_rank.sql
+++ sql/kpi_14_customer_revenue_rank.sql
@@ -1,1 +1,28 @@
+/* ===============================
+KPI 14: customer_revenue_rank
+Business Question:
+-Which customers generate the highest revenue?
+=============================== */
 
+with customer_revenue As(SELECT
+c.customer_id as customerid,
+sum(o.total_sales) as total_revenue
+FROM orders o 
+JOIN customers c
+on o.customer_id=c.customer_id
+group by c.customer_id
+)
+select 
+customerid,
+total_revenue,
+rank() over(ORDER BY total_reven desc) as rank
+from customer_revenue
+
+/*
+Insight:
+This analysis ranks customers based on the total revenue they generate.
+
+It helps identify high-value customers who contribute the most to overall sales.
+
+The results can support targeted marketing strategies and customer retention programs.
+*/

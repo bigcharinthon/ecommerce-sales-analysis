0e8bef964a249657bc87ad3f997d97dd14a13413
bigcharinthon <bigcharinthon124@gmail.com>
2026-03-11 14:26:56 +0700

    Update kpi_13_running_total_revenue.sql

--- sql/kpi_13_running_total_revenue.sql
+++ sql/kpi_13_running_total_revenue.sql
@@ -1,1 +1,25 @@
+/* ===============================
+KPI 12: revenue_growth_rate
+Business Question:
+-How does revenue grow month over month?
+=============================== */
 
+with monthly_sales AS (SELECT 
+      strftime('%Y-%m', order_date) AS month,
+      SUM(total_sales) AS monthly_sales
+    FROM orders
+    GROUP BY month
+)
+SELECT month,monthly_sales,
+sum(monthly_sales) OVER(ORDER BY month) AS total_revenue
+from monthly_sales
+
+/*
+Insight:
+This analysis calculates the month-over-month (MoM) revenue growth.
+
+It helps businesses track changes in sales performance between months.
+The results can help identify periods of growth or decline,
+
+supporting decisions such as promotional campaigns or sales strategies.
+*/

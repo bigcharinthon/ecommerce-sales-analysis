2b7d31766ba142e37ff17a3a5ea8bca01ef0ffd6
bigcharinthon <bigcharinthon124@gmail.com>
2026-03-11 13:28:22 +0700

    Update kpi_08_payment_method_usage.sql

--- sql/kpi_08_payment_method_usage.sql
+++ sql/kpi_08_payment_method_usage.sql
@@ -1,1 +1,20 @@
+/*
+KPI 8: Payment Method Usage
+Business Question:
+Which payment methods are used most frequently?
+*/
 
+SELECT
+    payment_method,
+    COUNT(*) AS payment_count
+FROM payments
+GROUP BY payment_method;
+
+/*
+Insight:
+This query analyzes the usage of different payment methods.
+It helps businesses understand customer payment preferences.
+
+The results can support decisions on optimizing payment
+options and improving the checkout experience.
+*/

642339c4b5e81357b0a1a495e87d1b234a29e83d
bigcharinthon <bigcharinthon124@gmail.com>
2026-03-11 13:00:11 +0700

    Update and rename kpi_03_top_5_products.sql to kpi_03_top_products.sql

---
+++ sql/kpi_03_top_products.sql
@@ -0,0 +1,22 @@
+/* ===============================
+   KPI 3: Top Products by Revenue
+   Business Question:
+   - Which are the top 5 products by revenue?
+   =============================== */
+
+SELECT
+    p.product_name,
+    SUM(total_sales) AS revenue
+FROM orders o
+JOIN products p
+    ON o.product_id=p.product_id
+GROUP BY p.product_name
+ORDER BY revenue DESC
+LIMIT 5;
+
+/*
+Insight:
+This query identifies the top 5 revenue-generating products.
+These products are key contributors to total sales and can be
+prioritized for marketing campaigns, promotions, or inventory planning.
+*/
